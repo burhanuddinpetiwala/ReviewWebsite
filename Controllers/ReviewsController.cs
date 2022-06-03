a3b07b5be6dbc6673758333faca7c8ee8473d1a0
Burhanuddin Petiwala <c0809178@mylambton.ca>
2022-06-03 15:57:55 -0400

    Add project files.

---
+++ Controllers/ReviewsController.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using ReviewWebsite.Models;
+using ReviewWebsite.models;
+
+namespace ReviewWebsite.Controllers
+{
+    public class ReviewsController : Controller
+    {
+        private ReviewWebsiteContext db = new ReviewWebsiteContext();
+
+        // GET: Reviews
+        public ActionResult Index(string Search, string filter)
+        {
+            var movie = db.Reviews.ToList();
+            if(string.IsNullOrEmpty(Search) && string.IsNullOrEmpty(filter))
+            {
+                return View(movie);
+            }
+            else if(filter == "All")
+            {
+                movie = db.Reviews.Where(q => q.Review_desc.Contains(Search) || q.Review_name.Contains(Search) || q.director.Contains(Search) || q.publications.Contains(Search)).ToList();
+            }
+            else if(filter == "Movie Name")
+            {
+                movie = db.Reviews.Where(q => q.Review_name.Contains(Search)).ToList();
+                return View(movie);
+            }
+            
+            return View(movie);
+        }
+       
+
+        // GET: Reviews/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Reviews/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,Review_name,star,movie_poster,director,publications,Review_desc")] Review review)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Reviews.Add(review);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(review);
+        }
+
+        // GET: Reviews/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Review review = db.Reviews.Find(id);
+            if (review == null)
+            {
+                return HttpNotFound();
+            }
+            return View(review);
+        }
+
+        // POST: Reviews/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,Review_name,star,movie_poster,director,publications,Review_desc")] Review review)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(review).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(review);
+        }
+
+        // GET: Reviews/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Review review = db.Reviews.Find(id);
+            if (review == null)
+            {
+                return HttpNotFound();
+            }
+            return View(review);
+        }
+
+        // POST: Reviews/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Review review = db.Reviews.Find(id);
+            db.Reviews.Remove(review);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ Models/Review.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace ReviewWebsite.Models
+{
+    public class Review
+    {
+      [Key]
+       public virtual int Id { get; set; }
+
+        [Required (ErrorMessage = "Please enter Movie Name")]
+        public virtual string Review_name { get; set; }
+
+        [Required (ErrorMessage = "Please rate the movie...")]
+        public virtual short star { get; set; }
+
+        public virtual string movie_poster { get; set; }
+
+        [Display(Name = "Director")]
+        public virtual string director { get; set; }
+
+        [Display(Name = "Publication")]
+        public virtual string publications { get; set; }
+
+        [Required (ErrorMessage = "Please describe your rating so others can be benefited.")]
+        public virtual string Review_desc { get; set; }
+
+        public Review() {}
+
+        public Review(string name, short rating, string img, string direct, string pub, string desc)
+        {
+            Review_name = name;
+            star = rating;
+            movie_poster = img;
+            director = direct;
+            publications = pub;
+            Review_desc = desc;
+        }
+
+    }
+}
